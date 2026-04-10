016e0bfb9b9a93d54aafd19966120092d48fd6cf
Hub-Browser <biyang.enterprise@gmail.com>
2026-04-10 12:36:00 +0600

    hw

---
+++ Module 1/Lesson 2/hw.py
@@ -0,0 +1,6 @@
+adam="January 1, 2012"
+tim="July 18, 2001"
+billy="December 31, 1998"
+naple="February 29, 2011"
+milo="September 11, 2009"
+print("Adam",adam,", Tim",tim,", Billy",billy,", Naple",naple,", Milo",milo)

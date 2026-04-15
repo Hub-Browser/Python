e4d3039f8be278554e7589fd9315e7f0476d2340
Hub-Browser <biyang.enterprise@gmail.com>
2026-04-15 16:30:16 +0600

    Activity 2

---
+++ Module 1/Lesson 3/Activity 2.py
@@ -0,0 +1,18 @@
+name="Penguin"
+age=15
+is_student=True
+weight=38.5
+print("Name:",name)
+print("Data type of name:",type(name))
+print("age:",age)
+print("Data type of age:",type(age))
+print("Is student:",is_student)
+print("Data type of Is_student:",type(is_student))
+print("Weight:",weight)
+print("Data type of Weight:",type(weight))
+weight=int(weight)
+print("Weight:",weight)
+print("Data type of weight:",type(weight))
+age=str(age)
+print("Data type of age:",type(age))
+

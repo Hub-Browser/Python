c48d541ea9742d3868af8cd8984f40fe87e7b392
Hub-Browser <biyang.enterprise@gmail.com>
2026-04-22 18:45:03 +0600

    hw

---
+++ Module 1/Lesson 6/extra1.py
@@ -0,0 +1,5 @@
+fire=input("Is there any fire or not (y/n)")
+if fire=="y":
+    print("Call the fire brigade")
+else:
+    print("No need to call the fire brigade") 
---
+++ Module 1/Lesson 6/hw.py
@@ -0,0 +1,5 @@
+a=input("Enter an alphabet")
+if "a"<=a<"z" or "A"<=a<"Z":
+    print(f"{a} is an alphabet")
+else: 
+    print(f"{a} is not an alphabet")

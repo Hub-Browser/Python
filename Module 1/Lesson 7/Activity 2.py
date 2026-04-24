6df480bb1041e1e574f36fb9a3040f6267440fc3
Hub-Browser <biyang.enterprise@gmail.com>
2026-04-24 12:28:41 +0600

    Activity 2

--- Module 1/Lesson 7/Activity 2.py
+++ Module 1/Lesson 7/Activity 2.py
@@ -1,5 +1,9 @@
-guestlist=["Alice","Bob","James","Jake","Steven"]
-if ("aarush" in guestlist):
-    print("aarush is in guest list")
-else:
-    print("aarush is not in guest list")
+a=10
+b=-10
+print("Right Shift")
+print("a >> 1:",a>>1)
+print("b >> 1:",b>>1)
+
+print("Left Shift")
+print("a << 2:",a<<2)
+print("b << 2:",b<<2)
---
+++ Module 1/Lesson 7/membership.py
@@ -0,0 +1,5 @@
+guestlist=["Alice","Bob","James","Jake","Steven"]
+if ("aarush" in guestlist):
+    print("aarush is in guest list")
+else:
+    print("aarush is not in guest list")

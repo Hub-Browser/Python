86893244549e46dba761311aea23f2e7cf9aada0
Hub-Browser <biyang.enterprise@gmail.com>
2026-04-24 12:47:42 +0600

    hw

---
+++ Module 1/Lesson 7/hw.py
@@ -0,0 +1,8 @@
+ascii=input("Enter a character: ")
+if len(ascii)==1:
+    ordd=ord(ascii)
+    typee=type(ascii)
+    print("Character ASCII value is",ordd)
+    print("Character type is",typee)
+    
+

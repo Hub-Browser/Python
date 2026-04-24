33a94a1f4ed7cae35602c38441df4fca9aee739b
Hub-Browser <biyang.enterprise@gmail.com>
2026-04-24 12:53:05 +0600

    hw

--- Module 1/Lesson 7/hw.py
+++ Module 1/Lesson 7/hw.py
@@ -1,8 +1,18 @@
 ascii=input("Enter a character: ")
 if len(ascii)==1:
     ordd=ord(ascii)
-    typee=type(ascii)
     print("Character ASCII value is",ordd)
-    print("Character type is",typee)
+    if ordd>=48 and ordd<=57:
+        print("Type: Digit")
+    elif ordd>=65 and ordd<=90:
+        print("Type: Uppercase Letter")
+    elif ordd>=97 and ordd<=122:
+        print("Type: Lowercase Letter")
+    elif ordd==32:
+        print("Type: Space")
+    else:
+        print("Type: Special Character")
     
+
+
 

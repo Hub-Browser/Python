07dbab4810d9536960d410ecb125fef86245e499
Hub-Browser <biyang.enterprise@gmail.com>
2026-04-29 16:21:43 +0600

    Activity 2

---
+++ Module 1/Lesson 8/Activity 2.py
@@ -0,0 +1,6 @@
+n1=int(input("Enter the numerator:"))
+n2=int(input("Enter the denominator:"))
+if n1%n2==0:
+    print(f"{n1} is divisible by {n2}")
+else:
+    print(f"{n1} is not divisible by {n2}")

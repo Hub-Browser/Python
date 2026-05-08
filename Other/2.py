cffe5226ee51bfee313ffba8b3621cc3637d5a48
Hub-Browser <biyang.enterprise@gmail.com>
2026-05-08 12:32:19 +0600

    Activity 2.py

--- Other/2.py
+++ Other/2.py
@@ -7,10 +7,13 @@ def date():
             return print("Not a valid date")
         mlist=["1","3","5","7","8","10","12"]
         d+=1
-        if (d==32 and m in mlist) or (d==31 and m not in mlist):
+        if (d==32 and m in mlist) or (d==31 and m not in mlist) or ((d==29 and m==2 and y%4==0) and (y%100!=0 or y%400==0)):
            d=1
            m+=1
            if m==13:
                m=1
                y+=1
+        print(f"")
+date()
+
 

e23aa89f1f634aa88a596d8e99c7639fb726001c
Hub-Browser <biyang.enterprise@gmail.com>
2026-05-13 16:23:58 +0600

    Activity 1

---
+++ Module 2/Lesson 5/extra.py
@@ -0,0 +1,7 @@
+
+r=5
+for i in range(0,r+1,1):
+    for j in range(i+1,r+1):
+        print(j,end="")
+    print()
+        
---
+++ Module 2/Lesson 6/Activity 1.py
@@ -0,0 +1,13 @@
+import turtle
+turtle.Screen().bgcolor("orange")
+turtle.Screen().setup(512,380)
+t=turtle.Turtle()
+
+no_s=6
+s_l=70
+a=360/no_s
+
+for i in range(no_s):
+    t.forward(s_l)
+    t.right(a)
+turtle.done()

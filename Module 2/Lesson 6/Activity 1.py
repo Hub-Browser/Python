99888ab2e91e44918c0b9e27efe65edc0d890b6d
Hub-Browser <biyang.enterprise@gmail.com>
2026-05-13 16:35:21 +0600

    Activity 2

--- Module 2/Lesson 6/Activity 1.py
+++ Module 2/Lesson 6/Activity 1.py
@@ -1,6 +1,6 @@
 import turtle
 turtle.Screen().bgcolor("orange")
-turtle.Screen().setup(512,380)
+turtle.Screen().setup(800,600)
 t=turtle.Turtle()
 
 no_s=6
@@ -10,4 +10,3 @@ a=360/no_s
 for i in range(no_s):
     t.forward(s_l)
     t.right(a)
-turtle.done()
---
+++ Module 2/Lesson 6/Activity 2.py
@@ -0,0 +1,18 @@
+import turtle
+turtle.Screen().bgcolor("lightblue")
+t=turtle.Turtle()
+
+for i in range(2):
+  t.forward(100)
+  t.left(120)
+t.forward(100)
+t.up()
+t.right(150)
+t.forward(50)
+t.right(90)
+t.down()
+for i in range(2):
+  t.forward(100)
+  t.right(120)
+t.forward(100)
+turtle.done()

06b9e4432e2d900c7021b1dfc105b91b07ff3fbc
Hub-Browser <biyang.enterprise@gmail.com>
2026-05-18 22:09:13 +0600

    hw

--- Module 3/Lesson 3/extra.py
+++ Module 3/Lesson 3/extra.py
@@ -1,15 +1,14 @@
-def exec():
-    listt=[]
-    def max():
-        try:
-            ll=int(input("Enter no of items in list:"))
-            for i in range(ll):
-                li=int(input("Enter list item:"))
-                listt.append(li)
-            listt.sort()
-            print(listt[ll-1])
-        except Exception as e:
-            print("Invalid input")
-    max()
+def max():
+    try:
+        ll=int(input("Enter no of items in list:"))
+        for i in range(ll):
+            li=int(input("Enter list item:"))
+            listt.append(li)
+        listt.sort()
+        print(listt[ll-1])
+    except Exception as e:
+        print("Invalid input")
+
 while True:
-    exec()
+    listt=[]
+    max()

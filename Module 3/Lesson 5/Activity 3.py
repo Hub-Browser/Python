06275696e79a1d1e8fa01aebbe60fb2759bb4ffe
Hub-Browser <biyang.enterprise@gmail.com>
2026-05-19 16:09:00 +0600

    Activity 3

---
+++ Module 3/Lesson 5/Activity 3.py
@@ -0,0 +1,12 @@
+import math as m
+
+print("The ceiling and floor value of 23.56")
+print(f"{m.ceil(23.56)} and {m.floor(23.56)}")
+
+x=10
+y=-15
+print(str(m.copysign(x,y)))
+
+print("Factorial of 5:",m.factorial(5))
+
+print("The GCD of 24 and 36:",m.gcd(24,36))

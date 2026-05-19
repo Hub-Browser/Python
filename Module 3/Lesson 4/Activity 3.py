498cf74d7a22a2e5cc101c2c98bbe0452f3115af
Hub-Browser <biyang.enterprise@gmail.com>
2026-05-19 15:36:11 +0600

    Activity 3

--- Module 3/Lesson 4/Activity 3.py
+++ Module 3/Lesson 4/Activity 3.py
@@ -1,7 +1,7 @@
 valid=False
 while not valid:
     try:
-        n=int(input("Enter a number"))
+        n=int(input("Enter a number:"))
         while n%2==0:
             print("Bye")
         valid=True

cf85707e760f0875356c2c1f0c278d23c662862e
Hub-Browser <biyang.enterprise@gmail.com>
2026-05-20 16:53:25 +0600

    x

---
+++ Module 3/Lesson 6/x.py
@@ -0,0 +1,30 @@
+def add(a,b):
+    return a+b
+def sub(a,b):
+    return a-b
+def mul(a,b):
+    return a*b
+def div(a,b):
+    return a/b
+
+while True:
+    try:
+        a=float(input("Enter the 2 numbers and the operation:\nNumber 1:"))
+        b=float(input("Number 2:"))
+        op=input("Operation:")
+    
+        if "+" in op:
+            print(add(a,b))
+        elif "-" in op:
+            print(sub(a,b))
+        elif "*" in op:
+            print(mul(a,b))
+        elif "/" in op:
+            print(round(div(a,b),3))
+        else:
+            print("Invalid input")
+
+    except ValueError:
+        print("That is not a number!")
+    except ZeroDivisionError:
+        print("You cannot divide by zero!")

bf5d65147eb25ab9f18ca13ce1e870d2b5319ea8
Hub-Browser <biyang.enterprise@gmail.com>
2026-05-23 12:49:59 +0600

    hw

---
+++ Module 4/Lesson 1/hw.py
@@ -0,0 +1,31 @@
+def sql():
+    try:
+        l=[]
+        le=[]
+        lo=[]
+        lb=int(input("Enter lower bound of list:"))
+        ub=int(input("Enter upper bound of list:"))
+        for i in range(lb,ub+1):
+            l.append(i**2)
+            if i%2==0:
+                le.append(i)
+            elif i%2!=0:
+                lo.append(i)
+            else:
+                print("Invalid")
+        print("Square values are:")
+        for i in l:
+            print(i,end=", ")
+        print("\nOdd values are:")
+        for i in lo:
+            print(i,end=", ")
+        print("\nEven values are:")
+        for i in le:
+            print(i,end=", ")
+        print("\n")
+    except:
+        print("Error")
+
+while True:
+    sql()
+        

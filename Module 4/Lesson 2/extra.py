8664142cf151024dad82810c6af2c9727e56f863
Hub-Browser <biyang.enterprise@gmail.com>
2026-05-26 12:34:40 +0600

    hw

---
+++ Module 4/Lesson 2/extra.py
@@ -0,0 +1,5 @@
+tuple=(35,47,99,24,19,77)
+sum=0
+for i in tuple:
+    sum+=i
+print("Average is:",round((sum/len(tuple)),2))
---
+++ Module 4/Lesson 2/hw.py
@@ -0,0 +1,9 @@
+tup1=(4,3,2,2,-1,18)
+tup2=(2,4,8,8,3,2,9)
+p1=1
+p2=1
+for i in tup1:
+    p1*=i
+for i in tup2:
+    p2*=i
+print(p1,p2)

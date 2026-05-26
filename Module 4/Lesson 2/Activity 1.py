a03cc6670ee77e1c8f9bab0d07946d43e37d29b1
Hub-Browser <biyang.enterprise@gmail.com>
2026-05-26 12:13:58 +0600

    Activity 1

---
+++ Module 4/Lesson 2/Activity 1.py
@@ -0,0 +1,14 @@
+tuple1=("tuple",False,40,30.5)
+print(tuple1)
+tuple2=(4,5,8,9)
+print(tuple2)
+
+tuple2=tuple2+(9,)
+
+tuple3=(50,39,86,50,47,50)
+print(tuple3.count(50))
+
+print(tuple1[1:3])
+
+for i in tuple2:
+    print(i)

14351ce7683f9776367b646d2b87ef1318cd6cf6
Hub-Browser <biyang.enterprise@gmail.com>
2026-05-30 12:45:49 +0600

    Activity 1

---
+++ Module 4/Lesson 2/extra1.py
@@ -0,0 +1,30 @@
+list=[(5,6),(5,7),(5,8),(6,10),(7,13)]
+listt=[]
+t1=list[0]
+t2=list[1]
+t3=list[2]
+t4=list[3]
+t5=list[4]
+
+if t1[0]==t2[0]:
+    list.append[(t1+(5,))]
+elif t1[0]==t3[0]:
+    list.append[(t1+t3[1])]
+elif t1[0]==t4[0]:
+    list.append[(t1+t4[1])]
+elif t1[0]==t5[0]:
+    list.append[(t1+t5[1])]
+elif t2[0]==t3[0]:
+    list.append[(t2+t3[1])]
+elif t2[0]==t4[0]:
+    list.append[(t2+t4[1])]
+elif t2[0]==t5[0]:
+    list.append[(t2+t5[1])]
+elif t3[0]==t4[0]:
+    list.append[(t3+t4[1])]
+elif t3[0]==t5[0]:
+    list.append[(t3+t5[1])]
+elif t4[0]==t5[0]:
+    list.append[(t4+t5[1])]
+print(listt)
+

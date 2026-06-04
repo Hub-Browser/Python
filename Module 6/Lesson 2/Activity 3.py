c0d8f4762490d5c36ed6bbbecb656dcf1efb2c0b
Hub-Browser <biyang.enterprise@gmail.com>
2026-06-04 11:30:30 +0600

    Activity 3

---
+++ Module 6/Lesson 2/Activity 3.py
@@ -0,0 +1,9 @@
+class pair_elements():
+    def twosum(self,nums,target):
+        lookup={}
+        for i,num in enumerate(nums):
+            if target-num in lookup:
+                return(lookup[target-num],1)
+            lookup[num]=i
+value=int(input("Enter the sum for which you want to make this search:"))
+print("index1=%d, index2=%d" % pair_elements().twosum((10,20,30,40,50,60,70),value))

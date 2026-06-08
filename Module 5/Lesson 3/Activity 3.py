cad30d0f0bc1c8c943f9d8e6a1252a7024e6060a
Hub-Browser <biyang.enterprise@gmail.com>
2026-06-08 17:33:42 +0600

    Activity 3

---
+++ Module 5/Lesson 3/Activity 3.py
@@ -0,0 +1,16 @@
+class person:
+    def __init__(self,name,id) -> None:
+        self.name=name
+        self.id=id
+    def display(self):
+        print(self.name)
+        print(self.id)
+class employee(person):
+    def __init__(self, name, id, salary, post) -> None:
+        self.salary=salary
+        self.post=post
+        super().__init__(name,id)
+obj=employee("Aarush",2026,300000,"Manager")
+obj.display()
+print(obj.salary)
+print(obj.post)

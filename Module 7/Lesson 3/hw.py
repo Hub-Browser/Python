17d8ac01bc395c20c14ff4c32edacd1a0f084584
Hub-Browser <biyang.enterprise@gmail.com>
2026-06-24 12:37:37 +0600

    hw

---
+++ Module 7/Lesson 3/hw.py
@@ -0,0 +1,27 @@
+from tkinter import *
+
+window=Tk()
+window.title("Length converter app")
+window.geometry("400x400")
+
+label=Label(text="Enter length in inchies to convert")
+entry=Entry()
+textbox=Text(fg="black")
+
+def convert():
+    length=entry.get()
+    try:
+        length=float(length)
+        cm=length*2.54
+    except:
+        textbox.insert(END,"Invalid input")
+    textbox.insert(END,f"{cm} cm")
+
+button=Button(text="Convert",command=convert)
+
+label.place(x=100,y=20)
+entry.place(x=100,y=50)
+button.place(x=100,y=80)
+textbox.place(y=120)
+
+window.mainloop()
